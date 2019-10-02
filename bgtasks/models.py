71194746e23489d4f343b6ec524f80bd7515de24
bekhzod.tillakhanov <bekhzod.tillakhanov@gmail.com>
2019-10-02 11:45:31 +0500

    Fix Remote field

--- bgtasks/models.py
+++ bgtasks/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class RemoteField(models.PositiveIntegerField):
-    def get_attname(self):
+    def get_column_name(self):
         return '%s_id' % self.name
+
+    def get_attname_column(self):
+        attname = self.get_attname()
+        column = self.get_column_name()
+        return attname, column
+
+
