bc0d294b5b0b18566369b0ffd75b9d2b86cff4c1
bekhzod.tillakhanov <bekhzod.tillakhanov@gmail.com>
2019-10-11 15:03:24 +0500

    Rename validate_task to serializer_class

--- bgtasks/tests/test_rest.py
+++ bgtasks/tests/test_rest.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from bgtasks.rest_framework.validators import serializer_class
+from bgtasks import serializer_class
 from ..rpc import rpc_tasks, RPCClient
 from ..constants import SUCCESS, FAIL
 
@@ -10,7 +10,7 @@ def task1(_):
 
 
 @rpc_tasks('task2')
-@validate_task()
+@serializer_class()
 def task2(_):
     return {'status': SUCCESS, 'data': 'Hi 2'}
 
@@ -21,30 +21,30 @@ class RPCTasksTest(TestCase):
         self.assertEqual(SUCCESS, response['status'])
         self.assertEqual('Hi 1', response['data'])
 
-    def test_validate_task_1(self):
+    def test_serializer_class_1(self):
         message = 'Expected a list of items but got type "int".'
         response = RPCClient().call('task2', {'ids': 1})
         self.assertEqual(FAIL, response['status'])
         self.assertEqual(message, response['data']['ids'][0])
 
-    def test_validate_task_2(self):
+    def test_serializer_class_2(self):
         message = 'Expected a list of items but got type "str".'
         response = RPCClient().call('task2', {'ids': 'hello'})
         self.assertEqual(FAIL, response['status'])
         self.assertEqual(message, response['data']['ids'][0])
 
-    def test_validate_task_3(self):
+    def test_serializer_class_3(self):
         message = 'This list may not be empty.'
         response = RPCClient().call('task2', {'ids': []})
         self.assertEqual(FAIL, response['status'])
         self.assertEqual(message, response['data']['ids'][0])
 
-    def test_validate_task_4(self):
+    def test_serializer_class_4(self):
         message = 'No data provided'
         response = RPCClient().call('task2', None)
         self.assertEqual(FAIL, response['status'])
         self.assertEqual(message, response['data']['non_field_errors'][0])
 
-    def test_validate_task_5(self):
+    def test_serializer_class_5(self):
         response = RPCClient().call('task2', {'ids': [1, 2, 3]})
         self.assertEqual(SUCCESS, response['status'])
